bbaed23a95c315bd0409f235ec6e83316858d285
VadimR89 <rabtsun.vadim@gmail.com>
2023-09-18 20:23:10 +0300

    1

--- main.py
+++ main.py
@@ -1,1 +1,61 @@
 from collections import UserDict
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        if not self.validate_phone():
+            raise ValueError("Invalid phone number format")
+
+    def validate_phone(self):
+        return len(self.value) == 10 and self.value.isdigit()
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        phone_obj = Phone(phone)
+        self.phones.append(phone_obj)
+
+    def remove_phone(self, phone):
+        for phone_obj in self.phones:
+            if phone_obj.value == phone:
+                self.phones.remove(phone_obj)
+
+    def edit_phone(self, old_phone, new_phone):
+        found = False
+        for phone_obj in self.phones:
+            if phone_obj.value == old_phone:
+                phone_obj.value = new_phone
+                found = True
+        if not found:
+            raise ValueError(f"Phone number '{old_phone}' not found in the record")
+
+    def find_phone(self, phone):
+        for phone_obj in self.phones:
+            if phone_obj.value == phone:
+                return phone_obj
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        if name in self.data:
+            return self.data[name]
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
